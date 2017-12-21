4b67875eecb28ea58f28256e398771290b3087ec
PLDaily <pcd12321>
2017-12-21 20:42:47 +0800

    use html view

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const session = require('koa-session')
-const views = require('koa-views')
+const render = require('koa-ejs')
 const bodyParser = require('koa-bodyparser')
 const router = require('./routers')
 const logger = require('./middleware/logger')
@@ -10,7 +10,13 @@ const app = new Koa()
 
 app.use(logger())
 app.use(bodyParser())
-app.use(views(path.join(__dirname, 'views'), {extension: 'ejs'}))
+render(app, {
+  root: path.join(__dirname, 'views'),
+  layout: 'template',
+  viewExt: 'html',
+  cache: false,
+  debug: false
+})
 
 app.keys = ['some secret hurr']
 
